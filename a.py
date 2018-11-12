86585033b4b31c739cb1c4464875afd45fefe07e
Gouwon <jskd2938@gmail.com>
2018-11-12 18:10:40 +0900

    181112 lecture

--- a.py
+++ a.py
@@ -1,10 +1,40 @@
-print("Hello, World!!")
+class TestClass:
+    name = "TEST"
 
-111111111111111111111111
-111111111111111111111111
-111111111111111111111111
-111111111111111111111111
-111111111111111111111111
-111111111111111111111111
+    def __init__(self):
+        print("kkkkkkkkkkkkk")
 
-print("1234")
+    def static_method():
+        print("STATIC!!")
+
+    def get_name(self):
+        return self.name
+
+    def area(self, x, y):
+        return x * y
+
+class Child(TestClass):
+    def __init__(self):
+        super().__init__()
+        print("My init!!!")
+
+    def get_name(self):
+        t = super().get_name()
+        return "Chiled Name : " + self.name
+
+    def area(self, x, y):
+        t = super().area(x, y)
+        return t / 2
+
+test = TestClass()
+child = Child()
+
+cmd = input("Input the function name>> ")
+
+getattr(test, cmd)() ## 클래스를 만들었을 때, 코드를 
+getattr(TestClass,'static_method')()
+
+# print(" >>>>>>> ", test.get_name(), child.get_name())
+
+# c = callable(test.get_name)
+# print(" >>>>>> ", c)
