5b158e28c5ea8479cddcfe8ab44f5fe6d55e1062
Gouwon <jskd2938@gmail.com>
2018-11-12 14:52:56 +0900

    first a.py

---
+++ blackjack.py
@@ -0,0 +1,52 @@
+shpae = [Clover, Heart, Spade, Diamond]
+number = [A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K]
+
+
+class Card
+    def __init__(self)
+    print(Start)
+
+    def name(self, shape, number)
+        self.shpae = shpae
+        self.number = number
+
+    def share(self)
+    print() #카드를 나눠준다
+
+    def HaveCard(self)
+    print() #카드가 2장임?
+
+    def sum(self)
+    print() #카드 숫자 더해
+
+    def __del__(self)
+    print("you win" or "you lose")
+
+class Dealder
+    def __init__(self)
+        return
+
+class Player
+    def __init__(self)
+        return
+
+person = [Dealder(), Player()]
+imput = [input usage"카드가 2장이냐?"]
+if have card <= 2:
+    sum
+else:
+    plus card
+    
+
+
+
+
+
+
+
+
+
+
+메모리상에 자식이 올라갔을때
+
+이 자식클래스들이 프로그램에 있는 cpu의 메모리에서 호출
