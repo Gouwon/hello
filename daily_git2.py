d6283a941344ea3342ced8d9d9222e626bdb0f25
Gouwon <jskd2938@gmail.com>
2018-11-15 11:48:02 +0900

    181115 lecture

--- daily_git2.py
+++ daily_git2.py
@@ -14,9 +14,12 @@ sa = sys.argv ## sys.argv[0] : 실행파일  sys.argv[1]:msg
 user_msg = input("Commit 메세지를 입력하세요. ")
 print(len(sa))
 print(user_msg)
+print(type(user_msg))
+print(len(user_msg))
 has_msg = len(sa) >= 2
 
-if has_msg != 1: #if len(sa) < 2:===> git.bash에서 실행할 때
+
+if has_msg: #if len(sa) < 2:===> git.bash에서 실행할 때
     msg_default = now.strftime('%Y-%m-%d lecture')
     os.system('git commit -am "{}"'.format(msg_default))
 
