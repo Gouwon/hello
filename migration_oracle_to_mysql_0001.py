e1cbd580abc838babef0cef63a851b263f188b20
mac <mac@macui-MacBook-Pro.local>
2018-12-29 20:28:20 +0900

    181229 Migration_oracle_to_mysql

--- migration_oracle_to_mysql_0001.py
+++ migration_oracle_to_mysql_0001.py
@@ -1,114 +1,16 @@
 import migration_oracle_to_mysql_util_0001 as mu
 
-
-
-source_tables ={"JOBS" : "JOB_ID, JOB_TITLE, MIN_SALARY, MAX_SALARY", 
-                "DEPARTMENTS" : "DEPARTMENT_ID, DEPARTMENT_NAME, MANAGER_ID", 
-                "EMPLOYEES" : "EMPLOYEE_ID, FIRST_NAME, LAST_NAME, EMAIL, PHONE_NUMBER, HIRE_DATE, JOB_ID, SALARY, COMMISSION_PCT, MANAGER_ID, DEPARTMENT_ID", 
-                "JOB_HISTORY" : "EMPLOYEE_ID, START_DATE, END_DATE, JOB_ID, DEPARTMENT_ID"}
-
-
-target_tables ={"Job" : '''id varchar(45) not null,
-                         title varchar(45) not null,
-                         min_salary int default 0,
-                         max_salary int default 0,
-                         primary key(id)''', 
-                "Department" : '''id int,
-                                 name varchar(45) not null,
-                                 manager_id int default 0,
-                                 primary key(id)''', 
-                "Employee" : '''id int default 0 not null,
-                                first_name varchar(45),
-                                last_name varchar(45) not null,
-                                email varchar(45) not null,
-                                tel varchar(45),
-                                hire_date datetime not null,
-                                job varchar(45) not null,
-                                salary int default 0,
-                                commission_pct float(4,2) default 0,
-                                manager_id int default 0,
-                                department int default 0,
-                                primary key(id)''', 
-                "JobHistory" : '''employee int not null,
-                                  start_date datetime not null,
-                                  end_date datetime not null,
-                                  job varchar(45) not null,
-                                  department int default 0,
-                                  primary key(employee, start_date)'''}
-
+target_to_source ={"Job" : "JOBS", "Department" : "DEPARTMENTS", "Employee" : "EMPLOYEES", "JobHistory": "JOB_HISTORY"}
 
 conn_mysql = mu.connect_mysql("dooodb")
 
 with conn_mysql:
     cur_mysql = conn_mysql.cursor()
 
-    for target_table_name in target_tables:
-        print(1)
-        sql_sp = "call sp_drop_fk_refs(" + '"' + target_table_name + '"' + ")"
-        print(sql_sp)
-        cur_mysql.execute(sql_sp)
-        print(2)
-        sql_drop = "drop table if exists " + target_table_name
-        print(sql_drop)
-        cur_mysql.execute(sql_drop)
-        print(3)
-        sql_create = 'create table ' + target_table_name + ' (' + target_tables[target_table_name] + ')'
-        cur_mysql.execute(sql_create)
-        print(4)
+    for target_table_name in target_to_source:
+        mu.create_table(target_table_name)
+        mu.set_data(cur_mysql, target_table_name)
 
-        if target_table_name == "Job":
-            conn_oracle = mu.connect_oracle()
-            with conn_oracle:
-                # cursor를 만들어줍니다
-                cursor = conn_oracle.cursor()
-                sql = 'select ' + source_tables["JOBS"] + ' from JOBS'
-                cursor.execute(sql)
-                rows = cursor.fetchall()
-            print(5)
-            sql_insert = "insert into " + target_table_name + "(id, title, min_salary, max_salary) values(%s, %s, %s, %s)"
-            print(6)
-            cur_mysql.executemany(sql_insert, rows)
-            print(7)
-        elif target_table_name == "Department":
-            conn_oracle = mu.connect_oracle()
-            with conn_oracle:
-                # cursor를 만들어줍니다
-                cursor = conn_oracle.cursor()
-                sql = 'select ' + source_tables["DEPARTMENTS"] + ' from DEPARTMENTS'
-                cursor.execute(sql)
-                rows = cursor.fetchall()
-            print(5)
-            sql_insert = "insert into " + target_table_name + "(id, name, manager_id) values(%s, %s, %s)"
-            print(6)
-            cur_mysql.executemany(sql_insert, rows)
-            print(7)
-        elif target_table_name == "Employee":
-            conn_oracle = mu.connect_oracle()
-            with conn_oracle:
-                # cursor를 만들어줍니다
-                cursor = conn_oracle.cursor()
-                sql = 'select ' + source_tables["EMPLOYEES"] + ' from EMPLOYEES'
-                cursor.execute(sql)
-                rows = cursor.fetchall()
-            print(5)
-            sql_insert = "insert into " + target_table_name + "(id, first_name, last_name, email, tel, hire_date, job, salary, commission_pct, manager_id, department) values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-            print(6)
-            print(sql_insert)
-            cur_mysql.executemany(sql_insert, rows)
-            print(7)
-        else:
-            conn_oracle = mu.connect_oracle()
-            with conn_oracle:
-                # cursor를 만들어줍니다
-                cursor = conn_oracle.cursor()
-                sql = 'select ' + source_tables["JOB_HISTORY"] + ' from JOB_HISTORY'
-                cursor.execute(sql)
-                rows = cursor.fetchall()
-            print(5)
-            sql_insert = "insert into " + target_table_name + "(employee, start_date, end_date, job, department) values(%s, %s, %s, %s, %s)"
-            print(6)
-            cur_mysql.executemany(sql_insert, rows)
-            print(7)
 
     cur_mysql.execute('''alter table Employee
                         add constraint uq_email unique (email)''')
--- migration_oracle_to_mysql_verify_0001.py
+++ migration_oracle_to_mysql_verify_0001.py
@@ -3,10 +3,6 @@ import migration_oracle_to_mysql_util_0001 as mu
 conn_oracle = mu.connect_oracle()
 conn_mysql = mu.connect_mysql("dooodb")
 
-table_name = 'Subject'
-column_name = "id, name, profm classroom"
-
-
 source_tables ={"JOBS" : "JOB_ID, JOB_TITLE, MIN_SALARY, MAX_SALARY", 
                 "DEPARTMENTS" : "DEPARTMENT_ID, DEPARTMENT_NAME, MANAGER_ID", 
                 "EMPLOYEES" : "EMPLOYEE_ID, FIRST_NAME, LAST_NAME, EMAIL, PHONE_NUMBER, HIRE_DATE, JOB_ID, SALARY, COMMISSION_PCT, MANAGER_ID, DEPARTMENT_ID", 
@@ -24,16 +20,11 @@ with conn_mysql:
     for target_to_source_name in target_to_source:
         mysql_cnt = mu.get_count(conn_mysql, target_to_source_name)
         mysql_smpls = mu.get_sample(conn_mysql, target_to_source_name, "5")
-        print(target_to_source_name)
-        print(mysql_cnt, mysql_smpls)
-        print(len(mysql_smpls))
            
         conn_oracle = mu.connect_oracle()
         with conn_oracle:
-            # cursor를 만들어줍니다
             cursor = conn_oracle.cursor()
             oracle_cnt = mu.get_count(conn_oracle, target_to_source[target_to_source_name])
-            print("------------------------", target_to_source[target_to_source_name],oracle_cnt, mysql_cnt)
 
             if oracle_cnt != mysql_cnt:
                 print("NOT VALIED!!!! \nmysql_cnt = ", mysql_cnt, "\noracle_cnt = ", oracle_cnt)
@@ -42,13 +33,12 @@ with conn_mysql:
                 print("Count is OK")
                 for i, j in enumerate(mysql_smpls):
                     oracle_smpls = mu.get_sample_verify1(conn_oracle, target_to_source[target_to_source_name], source_tables[target_to_source[target_to_source_name]], mysql_smpls, mysql_smpls[i][0], mysql_smpls[i][1] )
-                    print(">>>>>>>>>>>>>>>>>>>",mysql_smpls)
-                    print("<<<<<<<<<<<<<<<<<<<",oracle_smpls)
+                    print("mysql_smpls >>>>>>>>>>>>>>>>>>>>",mysql_smpls)
+                    print("====================================================================================")
+                    print("oracle_smpls <<<<<<<<<<<<<<<<<<<",oracle_smpls)
                     if mysql_smpls[i][0:] == oracle_smpls[0][0:]:
                         print("OOOOOKKKKK")
                     else:
                         print("NOT MATCHED!!!!!")
                         break
-        
-                    # rows = cursor.fetchall()
-
+    
