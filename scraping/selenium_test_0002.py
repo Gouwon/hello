3f23b794aff5732ce36c74d8f6b8c87dc9aa112f
Gouwon <jskd2938@gmail.com>
2019-01-10 13:16:16 +0900

    190110 lecture

--- scraping/selenium_test_0002.py
+++ scraping/selenium_test_0002.py
@@ -7,14 +7,21 @@ os = platform.system()
 
 if os == "Windows":
     driver = webdriver.Chrome('C:\Program Files (x86)\chromedriver_win32/chromedriver.exe')
+    ctrl = Keys.CONTROL
 elif os == "Darwin": 
     driver = webdriver.Chrome('/Users/mac/workspace/chromedriver')  # mac or linux
+    ctrl = Keys.COMMAND
+else:
+    print("Not supported OS")
+    exit()
 
 driver.get("https://www.google.com")
 time.sleep(2)
 
 inputElement = driver.find_element_by_name("q")
 inputElement.send_keys("세종대왕")
+inputElement.send_keys(ctrl, 'a')
+inputElement.send_keys(ctrl, 'v')
 inputElement.submit()        # cf.  inputElement.send_keys(Keys.RETURN)
 
 time.sleep(5)                # cf.  driver.implicitly_wait(5)
