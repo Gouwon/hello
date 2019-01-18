6be48e786bdb80f9c5362b7ab7138734402e0f4b
mac <mac@macui-MacBook-Pro.local>
2019-01-18 22:59:40 +0900

    190118 lecture

--- test.py
+++ test.py
@@ -1,41 +1,10 @@
-from openpyxl.chart import (
-    Reference, Series,
-    ScatterChart
-)
-import openpyxl
+a = "aaaa"
+b = "안녕하세요"
 
-book = openpyxl.Workbook()
-sheet = book.active
+print(len(a))
+print(len(b))
 
-rows = [
-    ['Size', 'Batch 1', 'Batch 2'],
-    [2, 40, 30],
-    [3, 40, 25],
-    [4, 50, 30],
-    [5, 30, 25],
-    [6, 25, 35],
-    [7, 20, 40],
-]
-for row in rows:
-    sheet.append(row)
+import sys
 
-chart = ScatterChart()
-chart.style = 13
-chart.x_axis.title = 'Size'
-chart.y_axis.title = 'Percentage'
-
-xvalues = Reference(sheet, min_col=1,
-			 min_row=2, max_row=7)
-
-for i in range(2, 4):
-    values = Reference(sheet, 
-				min_col=i, 
-				min_row=1, 
-				max_row=7)
-    series = Series(values, xvalues, 
-				title_from_data=True)
-    chart.series.append(series)
-
-sheet.add_chart(chart, "A10")
-
-book.save("./scraping/results/data/test.xlsx")
+print(sys.getsizeof(a))
+print(sys.getsizeof(b))
