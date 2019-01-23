d1666af3d05fdad22fc27ba8162f026f327d82ea
mac <mac@macui-MacBook-Pro.local>
2019-01-23 16:02:14 +0900

    190123 lecture

--- scraping/project_melon/main.py
+++ scraping/project_melon/main.py
@@ -1,6 +1,7 @@
 import melon_utils as mu 
 import codecs
 import csv
+
 mu.get_top100list()
 
 csvFile = codecs.open('./results/melon_top_100_list(20190123).csv', 'r', 'utf-8')
@@ -11,5 +12,9 @@ lst = []
 for row in reader:
     lst = (row[1], row[4])
     mu.insert_data_to_db("dooodb", sql, lst)
-    
+
 print("=============== MelList Insert Completed! =================")
+
+url = "https://www.melon.com/album/detail.htm"
+params = {"albumId" : albumno }
+html = mu.get_html(url, 'get', params=params)
