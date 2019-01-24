224be6c05722a8df4bd702838c86ab5b74f1dec8
mac <mac@macui-MacBook-Pro.local>
2019-01-24 16:33:53 +0900

    190124 lecture

--- scraping/project_melon/test.py
+++ scraping/project_melon/test.py
@@ -2,16 +2,4 @@ import melon_utils as mu
 import codecs
 import csv
 
-sql2 = 'insert into SongRank(SongId, rank, rankDate, likecnt) values(%s, %s, %s, %s)'
-csvFile2 = codecs.open('./results/melon_top_100_list(20190124).csv', 'r', 'utf-8')
-reader2 = csv.reader(csvFile2, delimiter=',', quotechar='"')
-
-import datetime
-now = datetime.datetime.now()
-n = now.strftime('%Y%m%d')
-
-for row in reader2:
-    lst = (row[1], row[0], '20190124', row[4])
-    mu.insert_data_to_db("dooodb", sql2, lst)
-
-print("=============== SongRank Insert Completed! =================")
+mu.get_top100list()
