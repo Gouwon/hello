fa2385a27f3ac4ab69287aa25877585e791e677f
Gouwon <jskd2938@gmail.com>
2020-01-21 14:41:48 +0900

    200121 django venv

--- django/django_test/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DjangoTestConfig(AppConfig):
-    name = 'django_test'
--- django/django_test/urls.py
+++
@@ -1,4 +0,0 @@
-from django.urls import path
-from django_test import views
-
-urlpatterns = [ path("", views.home, name="home"), ]
